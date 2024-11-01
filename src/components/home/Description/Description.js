07fd3e7a649fdcca3f0eab6d5883cbc14e33aed2
soualahmohammedzakaria <mz_soualahmohammed@esi.dz>
2024-11-01 23:26:52 +0100

    Fixed some styles

--- src/components/home/Description/Description.js
+++ src/components/home/Description/Description.js
@@ -20,7 +20,7 @@ export default function Description() {
           Your browser does not support HTML5 video.
         </video>
         <div className="description-content">
-          <h1 style={{fontSize: '2.2rem'}}>Welcome to <span className='title-name'>ECON Master</span></h1>
+          <h1 className='title'>Welcome to <span className='title-name'>ECON Master</span></h1>
           <p>Your go-to resource for 2CP Economics class materials. Here, you'll find everything you need to succeed in your studies from comprehensive course materials, video recordings, summaries to chapter and exam tests.</p>
         </div>
     </section>
